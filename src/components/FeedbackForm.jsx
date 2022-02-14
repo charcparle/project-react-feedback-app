2caf519340b9925b64f3e6ec58c3ee70f5c852f9
Charles Cheng <charles.yhcheng@gmail.com>
2022-02-13 22:53:11 -0800

    Removed warning by commenting out redundant import

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,6 +1,6 @@
 
 import { useState, useContext, useEffect } from "react";
-import Rating from "./RatingSelect";
+// import Rating from "./RatingSelect";
 import Card from "./shared/Card";
 import Button from "./shared/Button";
 import RatingSelect from "./RatingSelect";
